9d317a75ea279f0805ae2d93186144d6bb56b5b6
Edgar Quintana <edgarquintana984@hotmail.com>
2016-11-25 10:43:46 -0500

    json directo en render

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,10 +1,7 @@
 class ProductsController < ApplicationController
   def index
     @products = Product.all
-
-    respond_to do |format|
-      format.json {render json: @products}
-    end
+    render json: @products
   end
 
   def new
