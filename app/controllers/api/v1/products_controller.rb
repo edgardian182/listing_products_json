ea76ac36ea9915e83633ca0ede4aa565c55b126c
Edgar Quintana <edgarquintana984@hotmail.com>
2016-11-27 22:22:36 -0500

    Update and Destroy API Actions

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -14,6 +14,21 @@ class Api::V1::ProductsController < ApplicationController
     end
   end
 
+  def update
+    product = Product.find(params[:id])
+    if product.update(product_params)
+      render json: product, status: :ok
+    else
+      render json: product, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    product = Product.find(params[:id])
+    product.destroy
+    head 204
+  end
+
   private
   def product_params
     params.require(:product).permit(:name,:price)
